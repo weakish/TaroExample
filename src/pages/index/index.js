b2e7b8410e1723f68f995ed46b919403e4c5a6c8
Jang Rush <weakish@gmail.com>
2018-12-16 15:58:09 +0800

    list ticket info

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -49,7 +49,46 @@ export default class Index extends Component {
   render () {
     if (this.state.statusCode === 200) {
       return (
-        <Text>共有{this.state.list.length}种门票销售。</Text>
+        <View className='container'>
+          <View className='tickets'>
+            <Text className='heading'>选择票种</Text>
+            {this.state.list.map(ticket => {
+
+              let quotaMessage  
+              let description
+              if (ticket.quota === 0) {
+                quotaMessage = "售罄"
+                description = (
+                  <View className='ticketDescription'>
+                    <Text className='aboutType'>{ticket.description}</Text>
+                    <Text className='aboutQuota'>{quotaMessage}</Text>
+                  </View>
+                )
+              } else if (ticket.quota > 0 && ticket.quota <10) {
+                quotaMessage = `仅剩${ticket.quota}张`
+                description = (
+                  <View className='ticketDescription'>
+                    <Text className='aboutType'>{ticket.description}</Text>
+                    <Text className='aboutQuota'>{quotaMessage}</Text>
+                  </View>
+                )
+              } else {
+                description = (
+                  <View className='ticketDescription'>
+                    <Text className='aboutType'>{ticket.description}</Text>
+                  </View>
+                )
+              }
+              return (
+                  <View className='ticketAvailable'>
+                    <Text className='ticketLabel'>{ticket.label}</Text>
+                    {description}
+                    <Text className='ticketPrice'>{ticket.price} 元</Text>
+                  </View>
+              )
+            })}
+          </View>
+        </View>
       )
     } else {
       return (
