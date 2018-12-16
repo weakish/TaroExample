9e915d86c73b88c6c45428439bd17366a1e95d1e
Jang Rush <weakish@gmail.com>
2018-12-16 15:04:08 +0800

    get request error handling

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -13,7 +13,9 @@ export default class Index extends Component {
   constructor() {
     super(...arguments)
     this.state = {
-      list: []
+      list: [],
+      statusCode: 0,
+      ErrorMessage: "恭喜你踩到了代码的坑。快来这里报告吧：https://github.com/weakish/TaroExample/issues"
     }
   }
 
@@ -26,10 +28,14 @@ export default class Index extends Component {
       if (res.statusCode === 200) {
         console.log(res.data.length);
         this.setState({
-          list: res.data
+          statusCode: res.statusCode,
+          list: res.data,
         })
       } else {
-        console.log(res.statusCode)
+        this.setState({
+          statusCode: res.statusCode,
+          ErrorMessage: `很不幸，碰到了网络问题(${res.statusCode}错误)。建议过一会儿重试一下。`
+        })
       }
     });
   }
@@ -41,10 +47,15 @@ export default class Index extends Component {
   componentDidHide () {}
 
   render () {
-    console.log("logging")
-    return (
-      <Text>test: {this.state.list.length}</Text>
-    )
+    if (this.state.statusCode === 200) {
+      return (
+        <Text>共有{this.state.list.length}种门票销售。</Text>
+      )
+    } else {
+      return (
+        <Text>{this.state.ErrorMessage}</Text>
+      )
+    }
   }
 }
 
