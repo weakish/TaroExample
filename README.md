4c7039408742163e0f45a2bd659341be1e7c9988
Jang Rush <weakish@gmail.com>
2018-12-16 14:39:06 +0800

    get request

---
+++ README.md
@@ -0,0 +1,11 @@
+Enter fackapi subdirectory and run:
+
+  node app.js
+  
+In the root directory of project (TaroExample), run:
+
+  npm run dev:h5
+  
+Your browser need to enable CORS (via command line argument or plugin)
+
+
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,27 +2,48 @@ import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import './index.css'
 
+
+
 export default class Index extends Component {
 
   config = {
     navigationBarTitleText: '活动报名'
   }
 
-  componentWillMount () { }
+  constructor() {
+    super(...arguments)
+    this.state = {
+      list: []
+    }
+  }
 
-  componentDidMount () { }
+  componentWillMount () {}
+
+  componentDidMount () {
+    Taro.request({
+      url: 'http://127.0.0.1:8080/'
+    }).then(res => {
+      if (res.statusCode === 200) {
+        console.log(res.data.length);
+        this.setState({
+          list: res.data
+        })
+      } else {
+        console.log(res.statusCode)
+      }
+    });
+  }
 
   componentWillUnmount () { }
 
-  componentDidShow () { }
+  componentDidShow () {}
 
-  componentDidHide () { }
+  componentDidHide () {}
 
   render () {
+    console.log("logging")
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-      </View>
+      <Text>test: {this.state.list.length}</Text>
     )
   }
 }
